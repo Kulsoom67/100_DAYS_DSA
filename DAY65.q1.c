8c6b46d4cd1c671e38c742ea35263a5dee44b371
Kulsoom <kulsoom20011@gmail.com>
2026-04-09 14:28:21 +0530

    Create DAY65.q1.c

---
+++ DAY65.q1.c
@@ -0,0 +1,101 @@
+QUES1: Using DFS and parent tracking, detect if undirected graph has a cycle.
+  #include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph{
+    int numVertices;
+    struct Node** adjLists;
+    int* visited;
+};
+
+struct Node* createNode(int v){
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int vertices){
+    struct Graph* graph = malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    graph->adjLists = malloc(vertices * sizeof(struct Node*));
+    graph->visited = malloc(vertices * sizeof(int));
+
+    for(int i=0;i<vertices;i++){
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+
+    return graph;
+}
+
+void addEdge(struct Graph* graph,int src,int dest){
+
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+int DFS(struct Graph* graph,int vertex,int parent){
+
+    graph->visited[vertex] = 1;
+
+    struct Node* temp = graph->adjLists[vertex];
+
+    while(temp){
+
+        int adjVertex = temp->vertex;
+
+        if(!graph->visited[adjVertex]){
+            if(DFS(graph,adjVertex,vertex))
+                return 1;
+        }
+
+        else if(adjVertex != parent){
+            return 1;
+        }
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+int hasCycle(struct Graph* graph){
+
+    for(int i=0;i<graph->numVertices;i++){
+        if(!graph->visited[i]){
+            if(DFS(graph,i,-1))
+                return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main(){
+
+    struct Graph* graph = createGraph(5);
+
+    addEdge(graph,0,1);
+    addEdge(graph,1,2);
+    addEdge(graph,2,3);
+    addEdge(graph,3,1);
+
+    if(hasCycle(graph))
+        printf("Graph contains cycle\n");
+    else
+        printf("Graph does not contain cycle\n");
+
+    return 0;
+}
